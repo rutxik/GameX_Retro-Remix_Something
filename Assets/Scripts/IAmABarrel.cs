60e06b9025b4736639d7b9cbed2621a56e020840
Raya <88187585+RPC29@users.noreply.github.com>
2026-04-11 04:39:07 +0530

    Parry

--- Assets/Scripts/IAmABarrel.cs
+++ Assets/Scripts/IAmABarrel.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 public class IAmABarrel : MonoBehaviour
@@ -7,12 +9,31 @@ public class IAmABarrel : MonoBehaviour
 
     public bool AndThisIsWhetherIllExplode;
 
-    public bool TickThisOneIfIAmNotActuallyABarrel;
+    public bool TickThisOneIfIAmNotActuallyABarrelAndIGoLeft;
+    public bool TickThisOneIfIAmNotActuallyABarrelAndIGoRight;
+
+    Vector3 startpos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startpos = transform.position;
+        if (TickThisOneIfIAmNotActuallyABarrelAndIGoRight)
+        {
+            
+            transform.DOMove(startpos + new Vector3(1, 0, 0),0.5f).SetEase(Ease.OutCubic);
+            gameObject.GetComponent<SpriteRenderer>().DOFade(0,0.5f);
+            Destroy(gameObject, 2);
+            Destroy(gameObject.GetComponent<Rigidbody2D>());
+
+        }
+        if (TickThisOneIfIAmNotActuallyABarrelAndIGoLeft)
+        {
+            transform.DOMove(startpos + new Vector3(-1, 0, 0),0.5f).SetEase(Ease.OutCubic);
+            gameObject.GetComponent<SpriteRenderer>().DOFade(0,0.5f);
+            Destroy(gameObject, 2);
+            Destroy(gameObject.GetComponent<Rigidbody2D>());
+        }
     }
 
     // Update is called once per frame
@@ -31,4 +52,23 @@ public class IAmABarrel : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (TickThisOneIfIAmNotActuallyABarrelAndIGoLeft || TickThisOneIfIAmNotActuallyABarrelAndIGoRight) return;
+        if (collision.gameObject.layer == 14)
+        {
+            StartCoroutine(Parried());
+        }
+    }
+
+    IEnumerator Parried()
+    {
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(0.3f);
+        Time.timeScale = 1;
+        gameObject.layer = 14;
+        gameObject.GetComponent<Rigidbody2D>().linearVelocity = gameObject.GetComponent<Rigidbody2D>().linearVelocity * (-2);
+        gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
 }
