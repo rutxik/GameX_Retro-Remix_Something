438ed61e50d7cda74b2135ebc0931192cbc4d933
Raya <88187585+RPC29@users.noreply.github.com>
2026-04-10 19:02:34 +0530

    New dk attacks

--- Assets/Scripts/NewDonkBoss.cs
+++ Assets/Scripts/NewDonkBoss.cs
@@ -14,6 +14,8 @@ public class NewDonkBoss : MonoBehaviour
     bool attacking;
     public GameObject Barrel;
     public movement_dj Player;
+    public SpriteRenderer dksprite;
+    public Sprite[] sprites;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,9 +28,14 @@ public class NewDonkBoss : MonoBehaviour
     {
         if (!attacking)
         {
-            if (Random.Range(0,5) == 0)
+            int randomnumber = Random.Range(0, 60);
+            if (randomnumber == 0)
             {
                 StartCoroutine(JumpStompAttack());
+            }
+            if (randomnumber == 1)
+            {
+                StartCoroutine(BarrelThrowAttack());
             }
         }
     }
@@ -36,13 +43,29 @@ public class NewDonkBoss : MonoBehaviour
     IEnumerator JumpStompAttack()
     {
         attacking = true;
+        dksprite.sprite = sprites[4];
         SourceXLevel = transform.position.x;
         TargetXLevel = Mathf.Clamp(SourceXLevel + (SourceXLevel + (SourceXLevel - Player.transform.position.x) > 0? -4:4), GroundLevelAndLeftBoundary.position.x, JumpLevelAndRightBoundary.position.x);
         transform.DOMoveX(TargetXLevel,0.8f).SetEase(Ease.OutCirc);
         transform.DOMoveY(JumpLevelAndRightBoundary.position.y, 1.2f).SetEase(Ease.OutCirc);
         yield return new WaitForSeconds(1.2f);
+        dksprite.sprite = sprites[2];
         transform.DOMoveY(GroundLevelAndLeftBoundary.position.y, 0.3f).SetEase(Ease.OutBack);
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.4f);
+        attacking = false;
+        dksprite.sprite = sprites[1];
+    }
+
+    IEnumerator BarrelThrowAttack()
+    {
+        attacking = true;
+        dksprite.sprite = sprites[0];
+        yield return new WaitForSeconds(1);
+        dksprite.sprite = sprites[3];
+        yield return new WaitForSeconds(1);
+        dksprite.sprite = sprites[5];
+        yield return new WaitForSeconds(1);
+        dksprite.sprite = sprites[1];
         attacking = false;
     }
 }
